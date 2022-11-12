7d5f76aa862b1de7102824df3f3fe72edb28d498
khaipnp <khai.pnp@outlook.com>
2022-11-12 10:06:23 +0700

    first day with python

---
+++ Python1011.py
@@ -0,0 +1,28 @@
+# Course 1: Comment line
+print("------------Course 1: Comment line------------")
+print("Hello Word")
+
+# Course 2: Variable
+print("------------Course 2: Variable------------")
+_a2 = 19 #Init variable a with 19
+_b2 = 5
+_c2 = _a2 + _b2 * _b2
+print(_c2)
+
+# Course 3: Variable (Number)
+print("------------Course 3: Variable (Number)------------")
+# Integer
+_a3 = 4
+# Float
+_b3 = 1.96123532959195018614326353412351
+# Decimal
+## Get all (*) library decimal
+from decimal import *
+getcontext().prec = 30
+_c3 = Decimal(10)/Decimal(3)
+# Fraction
+## Get all (*) library fraction
+from fractions import *
+_d3 = Fraction(6,9)
+# Complex
+_e3 = complex(2,5)
